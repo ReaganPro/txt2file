a378496ceae9fc626fed3e6714e051661c2f4349
ReaganPro <85480984+ReaganPro@users.noreply.github.com>
2024-03-16 20:28:28 +0300

    Create vars.py

---
+++ vars.py
@@ -0,0 +1,19 @@
+#Variables
+botToken = '5849758695:AAHH3yDqFUAPGX3kclU0OOddSWUwchx0e0Y'
+
+#backup channel ID
+bci = '-1002115655400'
+
+#Available commands
+cdtxt = f"These are the available commands:\n\n/cmd - To see this message\n\n/start - start me up\n\n/ping - Check if I'm alive\n\n/vsn - See my deatails\n\n/upgrade - 🔓 unlock more features"
+bv = "Bot Version: 05.03.24.2216\nHost: @GreatTechInc\nPrice: #FREE\nAuthor: @ReaganPro and ChatGPT3.5"
+
+#Text file watermark
+wm = "Created by @Txt2file_bot on telegram - https://t.me/txt2file_bot - upgrade to remove this watermark\n"
+
+#upgrade msg
+upmsg ="More features are still being added by the developer, check back soon!\n\nYou can as well /donate to the developer, Thanks."
+
+
+#Donate Text
+dtt="Thanks for your generosity, this function is under development."
